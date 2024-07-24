8f17c2f35758a094d2d247b175709a667aba85df
Twist1154n <rethabile1154n@gmail.com>
2024-07-24 19:19:05 +0200

    IRepository and IService Completed to be used for local testing
    Signed-off-by: Twist1154n <rethabile1154n@gmail.com>

---
+++ src/main/java/za/ac/cput/repository/IRepository.java
@@ -0,0 +1,19 @@
+package za.ac.cput.repository;
+
+/**
+ * IRepository.java
+ *
+ * @author Rethabile Ntsekhe
+ * Student Num: 220455430
+ * @date 24-Jul-24
+ */
+
+public interface IRepository<T, ID> {
+    T create(T t);
+
+    T read(ID id);
+
+    T update(T t);
+
+    boolean delete(ID id);
+}
